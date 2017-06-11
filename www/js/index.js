458c07531bc2df05cd0a8076792c8946af62843b
Venil Noronha <venil.noronha@gmail.com>
2017-06-10 21:10:23 -0700

    Adds emergency section and menu.

--- www/js/index.js
+++ www/js/index.js
@@ -6,13 +6,49 @@ var app = {
 
     bindEvents: function() {
     	var _this = this;
-    	$('#splash').on('click', function() {
-    		_this.showMenu();
-    	});
+    	$('#splash').on('click', function() { _this.hideSplash(); });
+    	$('.fa-bars').on('click', function() { $('.page').fadeOut(); _this.showMenu(); });
+
+    	$('#micon-student').on('click', function() { $('.page').hide(); _this.showStudent(); _this.hideMenu(); });
+    	$('#micon-volunteer').on('click', function() { $('.page').hide(); _this.showVolunteer(); _this.hideMenu(); });
+    	$('#micon-support').on('click', function() { $('.page').hide(); _this.showSupport(); _this.hideMenu(); });
+    	$('#micon-emergency').on('click', function() { $('.page').hide(); _this.showEmergency(); _this.hideMenu(); });
+
+    	$('#emergency-send').on('click', function() { _this.sendEmergencyMessage(); });
+    },
+
+    hideSplash: function() {
+        $('#splash').hide('slide', { direction: 'left' }, 600);
     },
 
     showMenu: function() {
-        $('#splash').hide("slide", { direction: "left" }, 600);
+    	$('#menu').animate({
+    		marginTop: '0'
+    	}, 400);
+    },
+
+    hideMenu: function() {
+    	$('#menu').animate({
+    		marginTop: '-736px'
+    	}, 600);
+    },
+
+    showEmergency: function() {
+    	$('#emergency').show();
+    	$('#emergency-text').focus();
+    },
+
+    sendEmergencyMessage: function() {
+    	var text = $('#emergency-text').val();
+    	$('#emergency-text').val('').focus();
+    	var d = $('<div style="clear: both;margin-top: 10px;float: left;"></div>')[0];
+    	d.innerHTML = '<div style="float: left;margin-right: 10px;font-size: 16pt;width: 260px;background-color: #ecf0f1;color: black;border-radius: 10px;padding: 3px 10px;">' + text + '</div><div style="float: left;"><img src="img/lehman.jpg" style="border-radius: 30px; width: 60px; height: 60px;" /></div>';
+    	$('#emergency-messages').append(d);
+    	setTimeout(function(){
+    		var d = $('<div style="clear: both;margin-top: 10px;float: left;"></div>')[0];
+    		d.innerHTML = '<div style="float: left;"><img src="img/operator.jpg" style="border-radius: 30px; width: 60px; height: 60px;" /></div><div style="float: left;margin-left: 10px;font-size: 16pt;width: 260px;background-color: #ecf0f1;color: black;border-radius: 10px;padding: 3px 10px;">Oh, I\'m sorry. Don\'t worry, help is on your way.</div>';
+    		$('#emergency-messages').append(d);
+    	}, 1000);
     }
 
 };
